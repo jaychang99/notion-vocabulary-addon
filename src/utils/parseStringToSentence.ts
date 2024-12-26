fde735fc1ff3de711994cfc08b2b7e2b17366817
jaychang99 <jaychang99@gmail.com>
2024-12-26 21:54:54 +0900

    feat: remove apple podcast copyright text automatically

--- src/utils/parseStringToSentence.ts
+++ src/utils/parseStringToSentence.ts
@@ -1,14 +1,44 @@
 import { Sentence } from '@/types/sentence';
 
 export const parseStringToSentence = (value: string): Sentence => {
-  const punctuationRemovedSentence = value.replace(
-    /[.,\/#!$%\^&\*;:='"\`~]/g,
-    '',
+  // const punctuationRemovedSentence = value.replace(
+  //   /[.,\/#!$%\^&\*;:='"\`~]/g,
+  //   '',
+  // );
+
+  const parseValue = pipe(
+    removeApplePodcastSourceText,
+    truncateTrailingNewline,
+    removePunctuation,
   );
 
-  return punctuationRemovedSentence.split(' ').map((word, index) => ({
+  const parsedValue = parseValue(value);
+
+  return parsedValue.split(' ').map((word, index) => ({
     id: `${word}-${index}`, // This is a temporary solution to avoid using the same id for different words
     value: word,
     selected: false,
   }));
+};
+
+type Transformer = (value: string) => string;
+
+const pipe =
+  (...fns: Transformer[]) =>
+  (value: string) =>
+    fns.reduce((acc, fn) => fn(acc), value);
+
+const removePunctuation = (value: string) =>
+  value.replace(/[.,\/#!$%\^&\*;:='"\`~]/g, '');
+
+const truncateTrailingNewline = (value: string) => value.replace(/\n$/, '');
+
+// remove copyright text automatically added by Apple Podcast
+const removeApplePodcastSourceText = (value: string) => {
+  // remove phrase that starts with From and ends with This material may be protected by copyright.
+
+  return value.replace(
+    /From[\s\S]*?This material may be protected by copyright\.\s*/,
+    '',
+  );
 };
