dacb5d32eee7d082f86a2a8ac2f292101e6a430d
jaychang99 <jaychang99@gmail.com>
2025-02-02 17:02:26 +0900

    feat: change favicon, title, and description for both browsers and PWAs

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,21 +2,21 @@ import type { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
+    name: 'Notion Vocabulary Addon',
+    short_name: 'NVA',
+    description: 'addon for notion to help you learn vocabulary',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/notion-vocabulary-addon.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512x512.png',
+        src: '/notion-vocabulary-addon.png',
         sizes: '512x512',
         type: 'image/png',
       },
